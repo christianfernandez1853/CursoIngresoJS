3bed71f3453430470860cc7085bfbc44bf99d0bc
christianfernandez1853 <christianfernandez1853@gmail.com>
2020-07-15 00:16:57 -0300

    Fin de ejercicios E/S

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,14 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	let sueldo;
+	let resultado;
 
-	alert("ok");
+	sueldo = parseInt(document.getElementById("txtIdSueldo").value);
+
+	resultado = sueldo * 1.1; // CONSULTAR: Si pongo 100 me sale 110.00000000000001 
+
+	// resultado = ((sueldo * 10) / 100) + sueldo; --> me da correcto, why?
+
+	document.getElementById("txtIdResultado").value = resultado;
 }
